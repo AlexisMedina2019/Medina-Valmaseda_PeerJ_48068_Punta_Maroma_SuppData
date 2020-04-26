00af2dffd9161815e570b1205f121a6786c8234f
Alexis Medina <50713993+AlexisMedina2019@users.noreply.github.com>
2020-04-26 15:13:38 -0500

    Update PeerJ_Punta_Maroma.R

--- PeerJ_Punta_Maroma.R
+++ PeerJ_Punta_Maroma.R
@@ -82,7 +82,7 @@ plot_grid( graA11, graA12, labels = "AUTO",align = "v",label_size = 12, rel_heig
 # for each species by periods,
 # before the 1990s(represented by 1985 year) and 2019 
 library("readxl")
-ivi= read.csv("Data S3 IVI.csv")
+ivi<- read_xlsx("Data S3 IVI.xlsx")
 # trasformations
 ivi$Year = as.factor(ivi$Year)
 ivi$Year=factor(ivi$Year, levels = c("1985", "2019"))
